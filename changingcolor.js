f84be6e86212f21e6918bff237a59fd356d8421c
Matthew Baker <mbaker.ra@mysummitps.org>
2016-11-14 18:49:55 +0000

    color changing pages

---
+++ changingcolor.js
@@ -0,0 +1,8 @@
+// JavaScript File
+var bgColor = document.getElementById("bgColor");
+var colorChangeBtn = document.getElementById("colorChangeBtn")
+
+colorChangeBtn.addEventListener("click", function(){
+    document.body.style.backgroundColor = bgColor.value;
+});
+
